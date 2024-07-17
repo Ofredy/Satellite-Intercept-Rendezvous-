2cc6396c3cbce12e008b9091ab6141513a85f5d7
Ofredy <orellanafredy96@gmail.com>
2024-07-17 16:35:08 -0500

    r&v to orbital elements

--- orbital_mechanic_sims/project_predict/project_predict.py
+++ orbital_mechanic_sims/project_predict/project_predict.py
@@ -1,6 +1,4 @@
 ####################### File Description #######################
-#     File used for Project Predict.
-#
 #     For a number of unidentified space objects the three components of
 #     vector position and velocity are generated from radar observations (see
 #     Project SITE/TRACK). From this information you are required to find:
@@ -19,20 +17,62 @@
 #       away from the earth. 
 
 
-# Library Inputs
+# System Imports
+import math
+
+# Library Imports
 import numpy as np
+
+# Our Imports
+from sims_constants import *
 
 
 class ProjectPredict:
 
-    def __init__(self, position_vector: np.array, velocity_vector: np.array):
+    def __init__(self, position_vector: list, velocity_vector: list):
         
-        self.position_vector = position_vector
-        self.velocity_vector = velocity_vector
+        self.r = np.expand_dims(np.array(position_vector), axis=0)
+        self.v = np.expand_dims(np.array(velocity_vector), axis=0)
 
         self._find_orbital_parameters()
 
-
     def _find_orbital_parameters(self):
 
-        pass
+        # angular momentum
+        self.h = np.cross(self.r, self.v)
+
+        # semi-latus rectus 
+        self.p = (np.linalg.norm(self.h)**2) / GRAVITATIONAL_PARAMETER
+
+        # eccentricity
+        self.e = (1/GRAVITATIONAL_PARAMETER) * ((np.linalg.norm(self.v)**2 - (GRAVITATIONAL_PARAMETER/np.linalg.norm(self.r))) * self.r - (np.vdot(self.r, self.v)) * self.v)
+
+        # inclination
+        self.i = math.acos(np.vdot(self.h, k_unit) / np.linalg.norm(self.h))
+
+        # node vector
+        self.n = np.cross(k_unit, self.h)
+
+        # longitude of ascending node
+        self.omega = math.acos(np.vdot(self.n, i_unit) / np.linalg.norm(self.n))
+
+        if self.n[0][j_index] < 0:
+            self.omega = 2 * math.pi - self.omega
+
+        # argument of periapsis
+        self.w = math.acos(np.vdot(self.n, self.e) / (np.linalg.norm(self.n) * np.linalg.norm(self.e)))
+
+        if self.e[0][k_index] < 0:
+            self.w = 2 * math.pi - self.w
+
+        # true anomaly
+        self.v_o = math.acos(np.vdot(self.e, self.r) / (np.linalg.norm(self.e) * np.linalg.norm(self.r)))
+
+        if np.vdot(self.r, self.v) < 0:
+            self.v_0 = 2 * math.pi - self.v_o
+
+
+if __name__ == "__main__":
+
+    predict = ProjectPredict( [3*math.sqrt(3)/4, 3/4, 0], [-1/(2*math.sqrt(2)), math.sqrt(3)/(2*math.sqrt(2)), 1/math.sqrt(2)] )
+    
